7d7b699713552ccce06ef8eeabdf38d2dc78dda9
sapirca <sapir.caduri@gmail.com>
2025-05-27 12:39:58 +0700

    undoing ring 0

--- src/objects/ring-elements.ts
+++ src/objects/ring-elements.ts
@@ -1,9 +1,9 @@
-export const all = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+export const all = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 export const odd = [1, 3, 5, 7, 9, 11];
-export const even = [0, 2, 4, 6, 8, 10, 12];
-export const left = [0, 1, 2, 3, 4, 5, 6];
+export const even = [2, 4, 6, 8, 10, 12];
+export const left = [1, 2, 3, 4, 5, 6];
 export const right = [7, 8, 9, 10, 11, 12];
-export const center = [0, 4, 5, 6, 7, 8, 9];
+export const center = [4, 5, 6, 7, 8, 9];
 export const outer = [1, 2, 3, 10, 11, 12];
 
 export const segment_all = "all";
